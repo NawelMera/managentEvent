182b5a053b46b4beb2483691881b1f9306b95072
theoc <theo.colin@ig2i.centralelille.fr>
2025-11-26 11:19:37 +0100

    Ajout fonctions de validation des info formulaire login & subcscribe

--- src/app/container/login/login.component.ts
+++ src/app/container/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Login } from '@service/login/login';
 
 @Component({
   selector: 'app-login.component',
@@ -8,12 +9,20 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
+
 export class LoginComponent {
   LoginPassword: string = "";
   loginEmail: string = "";
+  login = new Login();
   Submit()
   {
     console.log("Email: " + this.loginEmail + " Password: " + this.LoginPassword);
+    //verification de la syntaxe de l'adresse email
+    console.log(this.login.isValidLoginForm(this.loginEmail, this.LoginPassword));
+    if(!this.login.isValidLoginForm(this.loginEmail, this.LoginPassword))
+    {
+      alert("Email ou mot de passe incorrect");
+    }
   }
   // constructeur Rooter
   constructor(private router : Router) {}
