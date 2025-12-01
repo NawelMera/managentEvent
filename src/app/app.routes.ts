1fc5850c1908f1d79d12fe0788c23933b7baaff7
NawelMera <nawel.merabet@ig2i.centralelille.fr>
2025-12-01 21:02:15 +0100

    delete settingd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from 'container/home/home.component';
 import { LoginComponent } from 'container/login/login.component';
-import { SettingsComponent } from 'container/settings/settings.component';
 import { SubscribeComponent } from 'container/subscribe/subscribe.component';
 import { UpdateEventComponent } from 'container/update-event/update-event.component';
 import { CreateEventComponent } from 'container/create-event/create-event.component';
@@ -17,7 +16,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'subscribe', component: SubscribeComponent },
 
-  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
   { path: 'event/update/:id', component: UpdateEventComponent, canActivate: [AuthGuard] },
   { path: 'event/create', component: CreateEventComponent, canActivate: [AuthGuard] },
   { path: 'tags', component: TagsComponent, canActivate: [AuthGuard] },
